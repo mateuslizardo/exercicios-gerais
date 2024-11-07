5ff8d4b87a3a614c12497fd8330ebfdebcfdd249
mateuslizardo <mateuslizardo@outlook.com>
2024-11-07 12:43:12 -0300

    bib1

---
+++ exercicios/03_bibliotecas/bibli_01/Respostas/Mateus/main.c
@@ -0,0 +1,10 @@
+#include "esfera_utils.h"
+#include <stdio.h>
+
+int main(){
+    float raio;
+    scanf("%f", &raio);
+    printf("Area: %.2f\n", calcula_area(raio));
+    printf("Volume: %.2f\n", calcula_volume(raio));
+    return 0;
+}
