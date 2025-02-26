a1542327542848da55244a496ade214163fbd7a7
mateuslizardo <mateuslizardo@outlook.com>
2025-02-25 21:01:00 -0300

    terminei pet opaco

--- 07_TAD_opaco/TAD opaco PET/Resultados/MateusLizardo/completo/brasileirao.c
+++ 07_TAD_opaco/TAD opaco PET/Resultados/MateusLizardo/completo/brasileirao.c
@@ -30,53 +30,57 @@ BRA *CriaCamp() {
 
 void RealizaRodada(BRA *br) {
     int i, j, gols1, gols2;
-    char time1[50], time2[50];
-
-    for (i = 0; i < br->numTimes/2; i++) {
-        scanf("%s %d x %d %s\n", time1, &gols1, &gols2, time2);
-        AtualizaGolsMarcados(ObtemTimeTabela(br->tabela, time1), gols1);
-        AtualizaGolsSofridos(ObtemTimeTabela(br->tabela, time1), gols2);
-        AtualizaGolsMarcados(ObtemTimeTabela(br->tabela, time2), gols2);
-        AtualizaGolsSofridos(ObtemTimeTabela(br->tabela, time2), gols1);
-        if (gols1 > gols2) {
-            AtualizaVitorias(ObtemTimeTabela(br->tabela, time1));
-            AtualizaDerrotas(ObtemTimeTabela(br->tabela, time2));
-        } else if (gols1 < gols2) {
-            AtualizaVitorias(ObtemTimeTabela(br->tabela, time2));
-            AtualizaDerrotas(ObtemTimeTabela(br->tabela, time1));
-        } else {
-            AtualizaEmpates(ObtemTimeTabela(br->tabela, time1));
-            AtualizaEmpates(ObtemTimeTabela(br->tabela, time2));
-        }
+    char *time1 = (char*)calloc(MAX_TIME_NOME, sizeof(char));
+    char *time2 = (char*)calloc(MAX_TIME_NOME, sizeof(char));
+    
+    scanf("%s %d x %d %s\n", time1, &gols1, &gols2, time2);
+    
+    AtualizaGolsMarcados(ObtemTimeTabela(br->tabela, time1), gols1);
+    AtualizaGolsSofridos(ObtemTimeTabela(br->tabela, time1), gols2);
+    AtualizaGolsMarcados(ObtemTimeTabela(br->tabela, time2), gols2);
+    AtualizaGolsSofridos(ObtemTimeTabela(br->tabela, time2), gols1);
+    if (gols1 > gols2) {
+        AtualizaVitorias(ObtemTimeTabela(br->tabela, time1));
+        AtualizaDerrotas(ObtemTimeTabela(br->tabela, time2));
+    } else if (gols1 < gols2) {
+        AtualizaVitorias(ObtemTimeTabela(br->tabela, time2));
+        AtualizaDerrotas(ObtemTimeTabela(br->tabela, time1));
+    } else {
+        AtualizaEmpates(ObtemTimeTabela(br->tabela, time1));
+        AtualizaEmpates(ObtemTimeTabela(br->tabela, time2));
     }
+    free(time1);
+    free(time2);
+    OrdenaTabela(br->tabela);
 }
 
 void RealizaCamp(BRA *br) {
     char opcao;
-    while (1) {
+    int quantPartidas = 0;
+    while (scanf("%c\n", &opcao) == 1) {
         printf("Menu:\n");
         printf("C - Continuar\n");
         printf("R - Retirar 2 times e seguir\n");
         printf("F - Finalizar\n\n");
                 
-        scanf("%c\n", &opcao);
+        
         if (opcao == 'C') {
-            RealizaRodada(br);
-            OrdenaTabela(br->tabela);
+            for(int i = 0; i < br->numTimes/2; i++)
+                RealizaRodada(br);            
             ImprimeTabela(br->tabela);
+            quantPartidas++;
         } else if (opcao == 'R') {
+            printf("Times a serem retirados:\n");
             RemoveTimesCamp(br);
-            OrdenaTabela(br->tabela);
+        }
+        
+        if (opcao == 'F' || quantPartidas == br->numRodadas) {
+            printf("Esta foi a tabela final:\n");
             ImprimeTabela(br->tabela);
-        } else if (opcao == 'F') {
-            OrdenaTabela(br->tabela);
-            FinalizaCamp(br);
             break;
         } 
 
-        if(br->saidas == br->numTimes) {
-            OrdenaTabela(br->tabela);
-            FinalizaCamp(br);
+        if(br->numTimes == 0) {
             break;
         }
         
@@ -88,7 +92,13 @@ void RemoveTimesCamp(BRA *br) {
     scanf("%s %s\n", time1, time2);
     RemoveTimeTabela(br->tabela, time1);
     RemoveTimeTabela(br->tabela, time2);
+    printf("Os times %s e %s se retiraram do campeonato\n", time1, time2);
     br->saidas += 2;
+    br->numTimes -= 2;
+    if(br->numTimes > 1){
+        RealizaRodada(br);
+        ImprimeTabela(br->tabela);
+    }
 }
 
 void DesalocaCamp(BRA *br) {
@@ -97,7 +107,7 @@ void DesalocaCamp(BRA *br) {
 }
 
 void FinalizaCamp(BRA *br) {
-    ImprimeTabela(br->tabela);
     ImprimePremiacao(br->tabela, br->premio);
+    printf("Fim do campeonato\n");
     DesalocaCamp(br);
 }
---
+++ 07_TAD_opaco/TAD opaco PET/Resultados/MateusLizardo/completo/main.c
@@ -0,0 +1,10 @@
+#include "brasileirao.h"
+
+int main()
+{
+    BRA *br;
+    br = CriaCamp();
+    RealizaCamp(br);
+    FinalizaCamp(br);
+    return 0;
+}
